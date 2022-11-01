a27577a63d96bda83825673a4b944bd24c19c154
kimjin9822 <kimjin9822@naver.com>
2022-11-01 19:30:18 +0900

    http연결 put,post,json,text

---
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,35 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//사용자가 요청 -> data (응답)
+
+@RestController
+public class HttpControllerTest {
+
+	@GetMapping("/http/get") //인터넷 브라우저 연결은 무조건 get요청밖에 할 수 없다.
+	public String getTest(Member m) { //MessageConverter (스프링부트)
+		return "get 요청: "+m.getId() +","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
+	}
+
+	@PostMapping("/http/post")
+	public String postTest(@RequestBody Member m) { //MessageConverter (스프링부트)
+		return "post 요청: "+m.getId() +","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
+
+	}
+	
+	@PutMapping("/http/put")
+	public String putTest() {
+		return "put 요청";
+	}
+
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+}
