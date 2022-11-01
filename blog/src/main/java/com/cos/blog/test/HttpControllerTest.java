ebe429dd872066a9f953acadcbe7f2ff53c750e9
kimjin9822 <kimjin9822@naver.com>
2022-11-01 19:47:10 +0900

    http 연습

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 //사용자가 요청 -> data (응답)
 @RestController
 public class HttpControllerTest {
