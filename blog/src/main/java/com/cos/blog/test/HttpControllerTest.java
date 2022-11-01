c0c441f8791913bd61c8a7827e3b6dd69c92d57c
kimjin9822 <kimjin9822@naver.com>
2022-11-01 19:34:22 +0900

    http이해 및 실습

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //사용자가 요청 -> data (응답)
-
 @RestController
 public class HttpControllerTest {
 
