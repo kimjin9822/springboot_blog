e9865a1743776ffb130b104030eeb052661b6741
kimjin9822 <kimjin9822@naver.com>
2022-11-02 19:09:11 +0900

    lombok 세팅 및 응용
    Merge branch 'master' of
    https://github.com/kimjin9822/springboot_blog.git

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -8,9 +8,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
+
 //사용자가 요청 -> data (응답)
 @RestController
 public class HttpControllerTest {
+	
+	private static final String TAG = "HttpControllerTest: ";
+	
+	@GetMapping("/http/lombok")
+	public String lomboktest() {
+		Member m = Member.builder().username("jin").password("111").email("jin").build();
+		System.out.println(TAG+"getter: "+m.getUsername());
+		m.setUsername("aaa");
+		System.out.println(TAG+"setter: "+m.getUsername());
+		return "lombok test 완료";
+	}
 
 	@GetMapping("/http/get") //인터넷 브라우저 연결은 무조건 get요청밖에 할 수 없다.
 	public String getTest(Member m) { //MessageConverter (스프링부트)
--- blog/src/main/java/com/cos/blog/test/Member.java
+++ blog/src/main/java/com/cos/blog/test/Member.java
@@ -1,41 +1,19 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data //getter와 setter동시 사용
+@AllArgsConstructor //전체생성자
+@NoArgsConstructor //빈생성자
+@Builder
 public class Member {
-	private int id;
+	private int id; // final은 불변표시 변하지않을 값표현
 	private String username;
 	private String password;
 	private String email;
-	
-	public Member(int id, String username, String password, String email) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	
 }
