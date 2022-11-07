0506d65d229d493ebe76dcf88e41c6967e46b9a0
kimjin9822 <kimjin9822@naver.com>
2022-11-07 13:28:12 +0900

    DB user테이블생성
    Merge branch 'master' of
    https://github.com/kimjin9822/springboot_blog.git

---
+++ blog/src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,36 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity //user 클래스가 MYSQL에 테이블이 생성
+public class User {
+
+	@Id //primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 BD의 넘버링 전략을 따라간다.
+	private int id; //시퀀스, auto_increment
+	
+	@Column(nullable = false, length = 20)
+	private String username; //아이디
+	
+	@Column(nullable = false, length = 100)
+	private String password; //패스워드
+	
+	@Column(nullable = false, length = 50)
+	private String email; // 이메일
+	
+	@ColumnDefault("'user'")
+	private String role; //Enum 쓰는게 더좋음
+	
+	@CreationTimestamp //시간 자동 입력
+	private Timestamp createDate;
+	
+}
