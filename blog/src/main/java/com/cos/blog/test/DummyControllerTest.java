c51886956dcd089747df5f39b4d70340d6395877
kimjin9822 <kimjin9822@naver.com>
2022-11-15 16:55:53 +0900

    paging 테스트
    Merge branch 'master' of
    https://github.com/kimjin9822/springboot_blog.git

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,9 +1,14 @@
 package com.cos.blog.test;
 
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +18,8 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
+
 
 @RestController
 public class DummyControllerTest {
@@ -20,6 +27,21 @@ public class DummyControllerTest {
 	@Autowired
 	private UserRepository userRepository;
 	
+	// 전체 검색
+	@GetMapping("/dummy/user")
+	public List<User> list(){
+		return userRepository.findAll();
+}
+	//한페이지당 2건의 데이터를 리턴
+	@GetMapping("/dummy/user/page")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Direction.DESC) Pageable pageable){
+		Page<User> pagingUser =  userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	//1개 리턴
 	@GetMapping("/dummy/user/{id}")
 	public User Detail(@PathVariable int id) {
 		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>(){
