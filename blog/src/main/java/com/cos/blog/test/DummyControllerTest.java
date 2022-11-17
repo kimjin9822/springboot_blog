0b565b18da2327fb058a808ffd0eb6f715988d70
kimjin9822 <kimjin9822@naver.com>
2022-11-17 18:17:11 +0900

    update 사용(put) , 더티채킹 사용(함수종료시 자동commit)

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,21 +4,24 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
-
-import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
 
 
 @RestController
@@ -26,6 +29,32 @@ public class DummyControllerTest {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Transactional // 더티체킹 (save안해도 업데이트가능하며 함수종료시 자동 commit (변경감지 -> DB수정))
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requsetUser) {
+		System.out.println("id : "+id);
+		System.out.println("password : "+requsetUser.getPassword());
+		System.out.println("email : "+requsetUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requsetUser.getPassword());
+		user.setEmail(requsetUser.getEmail());
+		return user;
+	}
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (Exception e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		return "삭제되었습니다. id : "+id;
+	}
+	
 	
 	// 전체 검색
 	@GetMapping("/dummy/user")
