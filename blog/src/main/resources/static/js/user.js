0088bed435ee1965cf27745abdcc26901ada2a16
kimjin9822 <kimjin9822@naver.com>
2022-11-19 19:09:53 +0900

    ajax 사용 및 사용이유 학습

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -12,7 +12,7 @@ let index = {
 			email: $("#email").val()
 		}
 		console.log(data);
-		$.ajax().done().fail();
+		$.ajax().done().fail(); //ajax 사용 이유 : 요청에 대한 응답을 data(json)를 받기 위함 (html X) , 비동기통신
 	}
 }
 index.init();
