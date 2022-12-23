c25cd28fca50c28cfade5d102c2842b4c1acc514
kimjin9822 <kimjin9822@naver.com>
2022-12-23 11:52:29 +0900

    @autowired 원리이해
    Merge branch 'master' of
    https://github.com/kimjin9822/springboot_blog.git

--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -1,6 +1,5 @@
 package com.cos.blog.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -8,23 +7,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.dto.ReplySaveRequestDto;
 import com.cos.blog.model.Board;
-import com.cos.blog.model.Reply;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import com.cos.blog.repository.ReplyRepository;
-import com.cos.blog.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service // 스프링이 컴포넌트 스캔을 통해 Bean 등록, IOC해줌
+@RequiredArgsConstructor
 public class BoardService { // 서비스 필요이유 : 여러개의 트랜잭션 관리,
 
-	@Autowired
-	private BoardRepository boardRepository;
-	
-	@Autowired
-	private ReplyRepository replyRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
+
+	private final BoardRepository boardRepository;
+
+	private final ReplyRepository replyRepository;
 
 	@Transactional // 전체 트랙잭션을 하나로 묶음
 	public void 글쓰기(Board board, User user) {
